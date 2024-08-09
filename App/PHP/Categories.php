9f7170688c583366fbeec97b6ef7f067251fb1e9
Adithya Dewmina <155904561+adithyadewmina@users.noreply.github.com>
2024-08-09 13:22:02 +0530

    change

---
+++ App/PHP/Categories.php
@@ -0,0 +1,27 @@
+<?php
+    // Dynamic Header
+    include("NavigationBar.php");
+    include("sideMenu.php");
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../CSS/Categories.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="content">
+        <div class="searchBox">
+            <input type="text" class="searchBar" placeholder="Enter district">
+            <img class="search" src="../Resource/Icons/search_327593.png" alt="search">
+        </div>
+        <div class="card">
+            <div class="cardImg"></div>
+            <label class="details" for="price">Rs. 220,000,000</label>
+        </div>
+    </div>
+</html>
---
+++ App/PHP/sideMenu.php
@@ -0,0 +1,25 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../CSS/sideMenu.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="sideMenu">
+        <button class="category" id="urban">Urban <img src="../Resource/Icons/arrow1.png" class="arrow" id="urbanArrow"/></button>
+        <div class="subCategory" id="urbanCat">
+            <button id="agriculture">Agriculture lands</button>
+            <button id="commercial">Commercial lands</button>
+            <button id="residential">Residential lands</button>
+        </div>
+        <button class="category" id="rural">Rural <img src="../Resource/Icons/arrow1.png" class="arrow" id="ruralArrow"/></button>
+        <div class="subCategory" id="ruralCat">
+            <button id="agriculture">Agriculture lands</button>
+            <button id="commercial">Commercial lands</button>
+            <button id="residential">Residential lands</button>
+        </div>
+    </div>
+    <script src="../JS/sideMenu.js"></script>
+</body>
+</html>
